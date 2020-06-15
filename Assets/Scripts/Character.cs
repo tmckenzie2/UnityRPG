3e4a30887d1fbbf74d9bb93443f91f871a536cd0
tmckenzie2 <38116502+tmckenzie2@users.noreply.github.com>
2020-06-15 03:02:39 -0700

    separated the player class into a player class with a base class of character

---
+++ Assets/Scripts/Character.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class Character : MonoBehaviour
+{
+     [SerializeField]
+    private float speed;
+
+    protected Vector3 change;
+
+    private Animator animator;
+
+
+    // Start is called before the first frame update
+    protected virtual void Start()
+    {
+        animator = GetComponent<Animator>();
+        
+    }
+
+    // Update is called once per frame
+    protected virtual void Update()
+    {  
+        AnimateMovement(change);
+    }
+
+    
+
+    public void AnimateMovement(Vector3 change)
+    {
+        if ((Vector3)change != Vector3.zero)
+        {
+            Move();
+            animator.SetFloat("x", change.x);
+            animator.SetFloat("y", change.y);
+            animator.SetBool("moving", true);
+        }
+        else
+        {
+            animator.SetBool("moving", false);
+        }
+
+    }
+
+    public void Move()
+    {
+        transform.Translate(change*speed*Time.deltaTime);
+
+    }
+}
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : Character
+{
+   
+    // Start is called before the first frame update
+    protected override void Start()
+    {
+        base.Start();
+        
+    }
+
+    // Update is called once per frame
+    protected override void Update()
+    {
+        GetInput();
+        base.Update();
+        
+    }
+
+    private void GetInput()
+    {
+        change = Vector3.zero;
+        change.x = Input.GetAxisRaw("Horizontal");
+        change.y = Input.GetAxisRaw("Vertical");
+    }
+}
